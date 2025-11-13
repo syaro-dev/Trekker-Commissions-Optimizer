5b355118b7f1de6abac54f2de03739fcc7971711
peteredric <imotosagiri@gmail.com>
2025-11-14 05:51:34 +0700

    Implement LoadDataAsync
    add LoadDataAsync methods in JsonDataSerializer.cs
    add try-catch wrap for SaveDataAsync and LoadDataAsync in JsonDataSerializer.cs
    
    modify throw exception for file not found in JsonDataProvider.cs and JsonDataSerializer.cs

--- CommissionsOptimizerLib.Data.Json/JsonDataSerializer.cs
+++ CommissionsOptimizerLib.Data.Json/JsonDataSerializer.cs
@@ -13,9 +13,41 @@ public readonly struct JsonDataSerializer
     public static async Task SaveTrekkersDataAsync(string filePath, List<TrekkerData> trekkers, CancellationToken token = default)
         => await SaveDataAsync(filePath, trekkers, token);
 
+    public static async Task<List<Commission>> LoadCommissionsDataAsync(string filePath, CancellationToken token = default)
+        => await LoadDataAsync<Commission>(filePath, token);
+
+    public static async Task<List<TrekkerData>> LoadTrekkersDataAsync(string filePath, CancellationToken token = default)
+        => await LoadDataAsync<TrekkerData>(filePath, token);
+
     private static async Task SaveDataAsync<T>(string filePath, List<T> data, CancellationToken token = default)
     {
-        using var stream = File.OpenWrite(filePath);
-        await JsonSerializer.SerializeAsync(stream, data, _options, token);
+        try
+        {
+            using var stream = File.OpenWrite(filePath);
+            await JsonSerializer.SerializeAsync(stream, data, _options, token);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+
+    private static async Task<List<T>> LoadDataAsync<T>(string filePath, CancellationToken token = default)
+    {
+        try
+        {
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException($"File not found: {filePath}", filePath);
+
+            using var stream = File.OpenRead(filePath);
+            var result = await JsonSerializer.DeserializeAsync<List<T>>(stream, _options, token);
+
+            return result ?? [];
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return [];
+        }
     }
 }
