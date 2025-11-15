1da75fd3be5850193c765da68b406e14a87b9e61
peteredric <imotosagiri@gmail.com>
2025-11-15 15:16:45 +0700

    Add Calculator Interface

---
+++ CommissionsOptimizerLib.Core/Interfaces/ICommissionsOptimizer.cs
@@ -0,0 +1,9 @@
+﻿using CommissionsOptimizerLib.Core.Enums;
+using CommissionsOptimizerLib.Core.Models;
+
+namespace CommissionsOptimizerLib.Core.Interfaces;
+
+public interface ICommissionsOptimizer
+{
+    public OptimizationResults CalculateCommissions(List<PlayerTrekkerData> playerTrekkers, List<RewardType> selectedMaterials, int tyrantLevel);
+}
