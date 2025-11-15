2265d52c1716d0d3d4b5b14cf0117f3fd72e2b87
peteredric <imotosagiri@gmail.com>
2025-11-15 14:38:53 +0700

    Google Sheets Integration
    add Data.GoogleSheets projects
    
    add GoogleSheetsCommissionsData.cs record
    add GoogleSheetsTrekkerData.cs record
    
    add GoogleSheetsDataProvider.cs class that provides data to the calculator
    
    add GoogleSheetsDataProviderTests.cs class that tests the data provider for Google Sheets
    
    modify CommissionsOptimizerService.cs CalculateCommissions to additionally require a tyrant auth level parameter

---
+++ CommissionsOptimizerLib.Data.GoogleSheets/Models/GoogleSheetsCommissionsData.cs
@@ -0,0 +1,23 @@
+﻿namespace CommissionsOptimizerLib.Data.GoogleSheets.Models;
+
+internal record GoogleSheetsCommissionsData
+{
+    public required string ID { get; set; }
+    public required string Name { get; set; }
+    public string? Role1 { get; set; }
+    public string? Role2 { get; set; }
+    public string? Role3 { get; set; }
+    public string? Personality1 { get; set; }
+    public string? Personality2 { get; set; }
+    public string? Personality3 { get; set; }
+    public int TyrantLvlReq { get; set; }
+    public int TrekkerLvlReq { get; set; }
+    public required string MainReward { get; set; }
+    public int MinAmt { get; set; }
+    public int? MaxAmt { get; set; }
+    public int MinBonusAmt { get; set; }
+    public int? MaxBonusAmt { get; set; }
+    public int? GiftsMinAmt { get; set; }
+    public int? GiftsMaxAmt { get; set; }
+    public bool Hidden { get; set; }
+}
